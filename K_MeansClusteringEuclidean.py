bb086cde239598099e37c471713197bb1539ff19
WouterKleynen <wouterkleynen@hotmail.com>
2022-11-07 22:12:17 +0100

    Updates

--- K_MeansClusteringEuclidean.py
+++ K_MeansClusteringEuclidean.py
@@ -5,13 +5,13 @@ import numpy as np
 class K_MeansClusteringEuclidean:
     
     def __init__(self, dataFilePath, K):
-        self.data                      = pd.read_csv(dataFilePath).to_numpy()
+        self.data                      = pd.read_csv(dataFilePath).to_numpy()[0:,1:]
         self.amountOfClusters          = K
         self.amountOfRows              = len(self.data)
         self.amountOfColumns           = len(self.data[0])
         self.practice_data             = self.data[0:self.amountOfRows,1:] # remove ID's
         self.columnsMaximaVector       = np.zeros(self.amountOfColumns-1)
-        self.centroids                 = np.zeros((self.amountOfClusters-1, self.amountOfColumns-1))
+        self.centroids                 = np.zeros((self.amountOfClusters-1,self.amountOfColumns-1))
         self.centroidToPointsDistances = np.zeros((len(self.practice_data), len(self.centroids)))
 
         
@@ -39,45 +39,39 @@ class K_MeansClusteringEuclidean:
                 
 
 
-# classInstance = K_MeansClusteringEuclidean('Dataset/EastWestAirlinesCluster.csv', 10)
-# classInstance.getMaximaColumns()
-# classInstance.updateCentroids()
-# classInstance.getDistances()
-# print(classInstance.distances)
-
 # Old code:
 
-# df = pd.read_csv('Dataset/EastWestAirlinesCluster.csv')
-# data = df.to_numpy()
+df = pd.read_csv('Dataset/EastWestAirlinesCluster.csv')
+data = df.to_numpy()
 
-# #Size of practice data
-# N = len(data)
-# M = len(data[0])
+#Size of practice data
+N = len(data)
+M = len(data[0])
 
-# #Define variable for K-means clustering
-# K = 9
+#Define variable for K-means clustering
+K = 9
 
-# #Initialize dataset and find maxima for all columns
-# practice_data = data[0:N,1:]
-# maxima = np.zeros(M-1)
-# for i in range(M-1):
-#     maxima[i] = max(practice_data[:,i])
+#Initialize dataset and find maxima for all columns
+practice_data = data[0:N,1:]
+maxima = np.zeros(M-1)
+for i in range(M-1):
+    maxima[i] = max(practice_data[:,i])
         
-# #Set centroids as evenly spaced accross all columns
-# centroids = np.zeros((K,M-1))    
-# for x in range(K):
-#     for i in range(M-1):
-#         centroids[x,i] = int(maxima[i]*x/K)    
+#Set centroids as evenly spaced accross all columns
+centroids = np.zeros((K,M-1))    
+for x in range(K):
+    for i in range(M-1):
+        centroids[x,i] = int(maxima[i]*x/K)    
 
-# #Define Euclidean distance
-# def Euclidean(a,b):
-#     dist = np.linalg.norm(a-b)
-#     return dist
+#Define Euclidean distance
+def Euclidean(a,b):
+    dist = np.linalg.norm(a-b)
+    return dist
 
-# #Compare all data to every centroid and see which is closest
-# distances = np.zeros((len(practice_data), len(centroids)))
-# for x in range (len(practice_data)):
-#     for i in range(len(centroids)):
-#         distances[x,i] = Euclidean(practice_data[x], centroids[i])
+#Compare all data to every centroid and see which is closest
+distances = np.zeros((len(practice_data), len(centroids)))
+for x in range (len(practice_data)):
+    for i in range(len(centroids)):
+        distances[x,i] = Euclidean(practice_data[x], centroids[i])
         
-# print(distances)
+print(distances)
