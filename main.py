1f9b64566d9e600609f3350adf522c858531e1a2
WouterKleynen <wouterkleynen@hotmail.com>
2022-11-08 17:39:00 +0100

    Implemented loss function that works!

--- main.py
+++ main.py
@@ -6,11 +6,22 @@ dataSetFilePath = 'Dataset/EastWestAirlinesCluster.csv'
 classInstance = K_MeansClusteringEuclidean(dataSetFilePath, K)
 classInstance.getMaximaOfColumns()
 classInstance.setStartCentroids()
+print(classInstance.lossFunction())
+
 classInstance.getDistanceOfPointsToCentroids()
 classInstance.setClusterDictionary()
 classInstance.updateCentroids()
+print(classInstance.lossFunction())
+
 classInstance.getDistanceOfPointsToCentroids()
 classInstance.setClusterDictionary()
 classInstance.updateCentroids()
+print(classInstance.lossFunction())
+
+classInstance.getDistanceOfPointsToCentroids()
+classInstance.setClusterDictionary()
+classInstance.updateCentroids()
+print(classInstance.lossFunction())
+
 
 
